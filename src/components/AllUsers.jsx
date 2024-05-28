e6a077e35a1ca86ac4833e4f7ce503e0040d8a0d
Kaustubh Anirudha Vaidya <140918319+KaustubhVaidya404@users.noreply.github.com>
2024-05-28 16:50:02 +0530

    update: added search user by name

--- src/components/AllUsers.jsx
+++ src/components/AllUsers.jsx
@@ -7,6 +7,8 @@ const url = "http://localhost:8080/api/v1/getall";
 
 const deleteurl = "http://localhost:8080/api/v1/delete"
 
+const getByNameUrl = "http://localhost:8080/api/v1/getByName"
+
 export default function AllUsers() {
 
     const [nextData, setNextData] = useState([]);
@@ -17,12 +19,6 @@ export default function AllUsers() {
         })
     }, [])
 
-    // function deleteUser(id) {
-    //     axois.delete(deleteurl, { params: { id: id } }).then((response) => {
-    //         console.log(response.data);
-    //         setNextData(nextData.filter((data) => data.id !== id));
-    //     })
-    // }
 
     function deleteUser(id) {
         axois.delete(deleteurl, { params: { id: id } })
@@ -35,10 +31,23 @@ export default function AllUsers() {
             });
     }
 
+    function searchUserByName(e) {
+        console.log(e.target.value)
+        axois.get(getByNameUrl, { params: { name: e.target.value } })
+            .then((response) => {
+                setNextData(response.data);
+            })
+            .catch((error) => {
+                console.error('Error searching user:', error);
+            });
+    }
 
     return (
         <div className="root">
-            <h1>User List</h1>
+            <div className="head">
+                <div className="sarchwrapper"><input type="text" onChange={searchUserByName} placeholder="search by name" className="saerchinput" /></div>
+                <div className="adduser"><button className="add">Add User</button></div>
+            </div>
             {nextData ? (
                 <table className="table">
                     <thead>
@@ -66,10 +75,16 @@ export default function AllUsers() {
                                 )
                             })}
                     </tbody>
+
                 </table>
             ) : (
                 <div>Loading...</div>
             )}
+            <div>
+                {
+                    nextData.length === 0 ? <div className="refreshwrapper"><button className="refreshbutton" onClick={() => { window.location.reload(); }}>Refresh</button></div> : null
+                }
+            </div>
         </div>
     )
 }
